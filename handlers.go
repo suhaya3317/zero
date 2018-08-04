adfbc5f3569ca2aa4e6648dd30483cde3e2ceea2
suhaya3317 <hayato_17n1200071@nnn.ed.jp>
2018-08-04 20:18:16 +0900

    Added json header

--- handlers.go
+++ handlers.go
@@ -17,6 +17,8 @@ func PostIndex(w http.ResponseWriter, r *http.Request) {
 		Post{Text: "Second Tweet!", Image: "https://example.com/example.png"},
 	}
 
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(posts); err != nil {
 		panic(err)
 	}
