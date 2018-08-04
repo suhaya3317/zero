751be75080599a283201740cccf96560f9c2eb8a
suhaya3317 <hayato_17n1200071@nnn.ed.jp>
2018-08-04 14:17:36 +0900

    Added appengine setting

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"net/http"
+	"log"
 	"fmt"
 	"html"
-	"log"
+	"google.golang.org/appengine"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
+	appengine.Main()
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
