2a01741fef2b906225fb996e2d435353bd878246
suhaya3317 <hayato_17n1200071@nnn.ed.jp>
2018-08-04 19:43:58 +0900

    Fixed func init()

--- main.go
+++ main.go
@@ -3,15 +3,11 @@ package main
 import (
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"google.golang.org/appengine"
 )
 
 func init() {
-	r := mux.NewRouter().StrictSlash(true)
-	r.HandleFunc("/", Index).Methods("GET")
-	r.HandleFunc("/posts", PostIndex).Methods("GET")
-	r.HandleFunc("/posts/{postId}", PostShow)
+	r := NewRouter()
 	http.Handle("/", r)
 }
 
